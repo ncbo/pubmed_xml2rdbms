e68143c6b652c197c9837a28e64c30421b2fe53c
Jennifer Vendetti <vendetti@stanford.edu>
2016-09-14 18:28:31 -0700

    Add database connection code

--- src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationTranslator.java
+++ src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationTranslator.java
@@ -3,9 +3,34 @@ package edu.stanford.ncbo.resourceindex.pubmed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MedlineCitationTranslator implements NewMedlineCitationEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(MedlineCitationTranslator.class);
+
+    private MedlineCitationETLProperties defaultProps;
+
+    private Connection connection;
+
+    public MedlineCitationTranslator() {
+        defaultProps = MedlineCitationETLProperties.getInstance();
+        getConnection();
+    }
+
+    private void getConnection() {
+        try {
+            String user = defaultProps.getProperty("jdbc.username");
+            String password = defaultProps.getProperty("jdbc.password");
+            String url = defaultProps.getProperty("jdbc.url");
+            Connection connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            logger.error("Can't connect to database: {}", e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void handleNewMedlineCitation(NewMedlineCitationEvent event) {
