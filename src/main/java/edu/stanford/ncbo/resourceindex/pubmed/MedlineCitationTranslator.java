4d74be596cec482490666cb7c4c8330a11662fe7
Jennifer Vendetti <vendetti@stanford.edu>
2016-09-22 17:16:22 -0700

    Add table insertion code

--- src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationTranslator.java
+++ src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationTranslator.java
@@ -3,58 +3,90 @@ package edu.stanford.ncbo.resourceindex.pubmed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.StringReader;
+import java.sql.*;
+import java.util.List;
 
 public class MedlineCitationTranslator implements NewMedlineCitationEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(MedlineCitationTranslator.class);
 
+    private Connection connection;
+
     private MedlineCitationETLProperties props;
 
-    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+    private String databaseName;
+
+    private String tableName;
 
     public MedlineCitationTranslator() {
         props = MedlineCitationETLProperties.getInstance();
-        getConnection();
-        initializeTable();
+        databaseName = props.getProperty("jdbc.dbName");
+        tableName = props.getProperty("jdbc.tableName");
+        initConnection();
+        initTable();
     }
 
     @Override
     public void handleNewMedlineCitation(NewMedlineCitationEvent event) {
         MedlineCitation citation = event.getMedlineCitation();
-        //logger.info("Write citation with ID: {} to database", citation.getPubMedId());
-    }
-
-    private void getConnection() {
-        String url = props.getProperty("jdbc.url");
-        String user = props.getProperty("jdbc.username");
-        String password = props.getProperty("jdbc.password");
 
         try {
-            connection = DriverManager.getConnection(url, user, password);
+            String id = citation.getPubMedId();
+            StringReader reader = new StringReader(id);
+            preparedStatement.setCharacterStream(1, reader, id.length());
+
+            preparedStatement.setString(2, citation.getArticleTitle());
+            preparedStatement.setString(3, citation.getAbstractText());
+
+            List keywordList = citation.getKeywords();
+            String keywords = (keywordList.isEmpty()) ? "" : String.join(",", keywordList);
+            preparedStatement.setString(4, keywords);
+
+            List meshHeadingsList = citation.getMeshHeadings();
+            String meshHeadings = (meshHeadingsList.isEmpty()) ? "" : String.join(",", meshHeadingsList);
+            preparedStatement.setString(5, meshHeadings);
+
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
+            connection.commit();
         } catch (SQLException e) {
             logSQLException(e);
         }
     }
 
-    private void initializeTable(){
-        String dbName = props.getProperty("jdbc.dbName");
-        String tableName = props.getProperty("jdbc.tableName");
-        Boolean reinitialize = Boolean.parseBoolean(props.getProperty("reinitializeTable"));
+    private void initConnection() {
+        String url = props.getProperty("jdbc.url");
+        String user = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+        String insertString =
+                String.format("INSERT INTO %s.%s (local_element_id, pm_title, pm_abstract, " +
+                        "pm_keywords, pm_meshheadings) VALUES (?,?,?,?,?)", databaseName, tableName);
 
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(insertString);
+        } catch (SQLException e) {
+            logSQLException(e);
+        }
+    }
+
+    private void initTable(){
+        Boolean reinitialize = Boolean.parseBoolean(props.getProperty("reinitializeTable"));
         String createString =
                 String.format("CREATE TABLE %s.%s (id integer(10) UNSIGNED NOT NULL AUTO_INCREMENT, " +
                         "local_element_id varchar(255) NOT NULL, pm_title text, pm_abstract text, " +
                         "pm_keywords text, pm_meshheadings text, PRIMARY KEY (id), " +
-                        "UNIQUE KEY local_element_id (local_element_id))", dbName, tableName);
+                        "UNIQUE KEY local_element_id (local_element_id))", databaseName, tableName);
+        String dropString = String.format("DROP TABLE IF EXISTS %s.%s", databaseName, tableName);
 
         try {
             Statement statement = connection.createStatement();
             if (reinitialize) {
-                statement.executeUpdate(String.format("DROP TABLE IF EXISTS %s.%s", dbName, tableName));
+                statement.executeUpdate(dropString);
             }
             statement.executeUpdate(createString);
         } catch (SQLException e) {
@@ -67,7 +99,7 @@ public class MedlineCitationTranslator implements NewMedlineCitationEventListene
             logger.error("SQLState: {}", ((SQLException)e).getSQLState());
             logger.error("Error Code: {}", ((SQLException)e).getErrorCode());
             logger.error("Message: {}", e.getMessage());
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
