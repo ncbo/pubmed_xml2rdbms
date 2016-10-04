29330badbe417830177a61d9c6e8923c3db2e2b9
Jennifer Vendetti <vendetti@stanford.edu>
2016-10-03 18:09:01 -0700

    Remove listener after all documents processed

--- src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationETL.java
+++ src/main/java/edu/stanford/ncbo/resourceindex/pubmed/MedlineCitationETL.java
@@ -88,6 +88,7 @@ public class MedlineCitationETL {
         }
 
         handler.removeNewMedlineCitationListener(translator);
+        handler.removeMedlineDocumentEndListener(translator);
 
         return true;
     }
